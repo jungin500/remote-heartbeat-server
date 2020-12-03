3f4d691ae5fae356c2c81c5d74a520ea383cf5f7
LimeOrangePie <ji5489@gmail.com>
2020-12-03 22:07:31 +0900

    Change port behaviour
    Signed-off-by: LimeOrangePie <ji5489@gmail.com>

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const express = require('express')
 const ping = require ("net-ping")
+const isPortReachable = require('is-port-reachable');
 
 const app = express()
 const pingSession = ping.createSession({
 	retries: 3,
-	timeout: 1000
+	timeout: 100
 })
 
 const SERVER_PORT = 8080
+const checkCommonlyUsedPortsOpened = async (ip_address) => {
+    const portList = [80, 22, 3389, 443, 41222, 8080, 8443]
+    
+    for(const port of portList)
+        if(await isPortReachable(port, { host: ip_address, timeout: 100 }))
+            return true;
+            
+    return false;
+}
 
 app.get('/heartbeat/:ip_address', (req, res) => {
     const ip_address = req.params.ip_address
 
-    pingSession.pingHost(ip_address, (error, target) => {
+    pingSession.pingHost(ip_address, async (error, target) => {
         if(error) {
-            if (error instanceof ping.RedirectReceivedError) {
+            if (await checkCommonlyUsedPortsOpened(ip_address)) {
+                res.status(200);
+                res.send({ result: true, message: target + " is alive" });
+                return;
+            } else if (error instanceof ping.RedirectReceivedError) {
                 res.status(200);
                 res.send({ result: true, message: target + " is not receiving but redirecting itself!" });
+                return;
             }
 
             res.status(400);
